ca87f36df31f900e4e402e4835e0267c5fb6ec12
pikoptr <tzykoko1@gmail.com>
2025-02-18 14:48:36 +0700

    hari 2

--- app/src/main/java/com/example/ukk/RegisterActivity.java
+++ app/src/main/java/com/example/ukk/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.ukk;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,21 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText registerUsername, registerPassword;
-    private Button registerButton;
+    private EditText registerNama, registerUsername, registerPassword, registerKonfirmasiPassword;
+    private Button registerButton, kembali;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        registerNama = findViewById(R.id.registerNama);
         registerUsername = findViewById(R.id.registerUsername);
         registerPassword = findViewById(R.id.registerPassword);
+        registerKonfirmasiPassword = findViewById(R.id.registerKonfirmasiPassword);
         registerButton = findViewById(R.id.registerButton);
+        kembali = findViewById(R.id.kembali);
 
         registerButton.setOnClickListener(v -> {
+            String Nama= registerNama.getText().toString();
             String username = registerUsername.getText().toString();
             String password = registerPassword.getText().toString();
+            String KonfirmasiPassword = registerKonfirmasiPassword.getText().toString();
 
             // Proses pendaftaran
             // Biasanya akan dihubungkan dengan database untuk menyimpan data pengguna baru
